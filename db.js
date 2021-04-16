58e44e773c556c214dc3a72fc30842a5fd110cdc
mfahmialkautsar <mfahmiak@gmail.com>
2021-04-16 22:49:12 +0700

    Disable rejectUnauthorized

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
-		rejectUnauthorized: process.env.DATABASE_URL ? true : false,
+		rejectUnauthorized: false,
 	},
 });
 
